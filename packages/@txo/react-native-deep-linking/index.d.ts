40d8e7818dd7119f74528e082c0415709eab7dc9
Erik Slovak <erik.slovak@technologystudio.sk>
2021-05-12 17:04:19 +0200

    add typescript type definitions

--- packages/@txo/react-native-deep-linking/index.d.ts
+++ packages/@txo/react-native-deep-linking/index.d.ts
@@ -1,2 +1,20 @@
-declare module "@txo/react-native-deep-linking" {
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2021-05-12T11:05:52+02:00
+ * @Copyright: Technology Studio
+**/
+
+import { ReactNode } from 'react'
+import { NavigationAction } from '@txo-peer-dep/react-conditional-navigation'
+
+declare module '@txo/react-native-deep-linking' {
+  type DeeplinkNavigationActionCreator = (params: Record<string, string>) => NavigationAction | null
+
+  type DeeplinkNavigationMap = Record<string, DeeplinkNavigationActionCreator>
+
+  type DeeplinkContainerProps = {
+    children: ReactNode,
+    deeplinkNavigationMap: DeeplinkNavigationMap | null,
+  }
+  const DeeplinkContainer: React.ComponentType<DeeplinkContainerProps>
 }
